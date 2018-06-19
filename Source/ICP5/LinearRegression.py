2a3312db99c692fe6f2155c6badfb91cefd21fe6
Bhargavi Nadendla <bhargavinadendla@gmail.com>
2018-06-19 18:31:07 -0500

    ICP5

--- Source/ICP5/LinearRegression.py
+++ Source/ICP5/LinearRegression.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x=np.array([0,1,2,3,4,5,6,7,8,9]) # Reading input X as an array
-y=np.array([1,3,2,5,7,8,8,9,10,12]) # Reading input Y as an array
-xMean=np.mean(x) #Calculating the mean of x
-yMean=np.mean(y) #Calculating the mean of y
-B1=(np.sum((x-xMean)*(y-yMean)))/(np.sum(np.power((x-xMean),2))) #Calculate the y intercept using the defined formula
-B0=yMean-(B1*xMean) #Calculate the slope using the defined formula
-plt.scatter(x,y) #plot x and y using matplotlib
-ry=B1*x+B0 #Applying linear regression with the equation y=mx+c
-plt.plot(x,ry) #Plotting the linear regression model
-plt.show() #showing the plot
+x=np.array([0,1,2,3,4,5,6,7,8,9])                 # Reading input X as an array
+y=np.array([1,3,2,5,7,8,8,9,10,12])              # Reading input Y as an array
+xMean=np.mean(x)                               #Calculating the mean of x
+yMean=np.mean(y)                               #Calculating the mean of y
+B1=(np.sum((x-xMean)*(y-yMean)))/(np.sum(np.power((x-xMean),2)))           #Calculate the y intercept using the defined formula
+B0=yMean-(B1*xMean)                             #Calculate the slope using the defined formula
+plt.scatter(x,y)                                #plot x and y using matplotlib
+ry=B1*x+B0                                      #Applying linear regression with the equation y=mx+c
+plt.plot(x,ry)                                 #Plotting the linear regression model
+plt.show()                                     #showing the plot
--- Source/ICP5/T-shir_size.py
+++ Source/ICP5/T-shir_size.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
-from sklearn.cluster import KMeans #importing KMeans from sklearn
-random.seed(2) #setting seed so that same random numbers would be generated
-height=[round(random.uniform(4,7),2) for i in range(20)] #dynamically generating heights within 4 t 7
-weight =[random.randint(50,70) for j in range(20)] #generating weights within 50 to 70
-X = np.vstack((height, weight)).T # combining to 2D array using vertical stack
+from sklearn.cluster import KMeans                #importing KMeans from sklearn
+random.seed(2)                                 #setting seed so that same random numbers would be generated
+height=[round(random.uniform(4,7),2) for i in range(20)]                 #dynamically generating heights within 4 t 7
+weight =[random.randint(50,70) for j in range(20)]                        #generating weights within 50 to 70
+X = np.vstack((height, weight)).T                                # combining to 2D array using vertical stack
 print(X)
-kmeans = KMeans(n_clusters=3) #Defining KMeans model with 3 clusters
-kmeans.fit(X)  #Applying KMeans
-plt.scatter(X[:,0], X[:,1], c=kmeans.labels_, cmap='rainbow') # scatterplot by grouping clusters with colors
-plt.scatter(kmeans.cluster_centers_[:,0] ,kmeans.cluster_centers_[:,1], color='black') #Plotting the centroid of each cluster in black
+kmeans = KMeans(n_clusters=3)                                   #Defining KMeans model with 3 clusters
+kmeans.fit(X)                                                   #Applying KMeans
+plt.scatter(X[:,0], X[:,1], c=kmeans.labels_, cmap='rainbow')               # scatterplot by grouping clusters with colors
+plt.scatter(kmeans.cluster_centers_[:,0] ,kmeans.cluster_centers_[:,1], color='black')             #Plotting the centroid of each cluster in black
 plt.show()
